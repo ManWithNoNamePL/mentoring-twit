46f834e6f72dd21311c5dac251d63ac66a72e27e
KATE\Екатерина <ponyataevaea@gmail.com>
2018-12-06 19:00:39 +0100

    fixed error message for 500 err code

--- core/src/main/java/com/tweet/core/error/ErrorCode.java
+++ core/src/main/java/com/tweet/core/error/ErrorCode.java
@@ -4,7 +4,7 @@ package com.tweet.core.error;
 public enum ErrorCode {
     USER_NOT_FOUND(404, "The user with name [%s] is not found"),
     TWEET_NOT_FOUND(404, "The tweet with id [%s] is not found"),
-    GENERAL_ERROR(500, "Happened something wrong");
+    GENERAL_ERROR(500, "Unexpected server behaviour");
 
     private final int code;
     private final String msg;
